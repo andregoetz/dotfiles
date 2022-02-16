e833f78a5470f57cd60dcb544de3410fd695bb92
andregoetz <andregoetz37@gmail.com>
2022-02-16 23:14:27 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo pacman -S zsh gvim neovim python-pynvim vim-spell-de vim-spell-en
+if [[ $1 == "-i" ]]; then
+    sudo pacman -S zsh gvim neovim python-pynvim vim-spell-de vim-spell-en
+fi
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 mkdir -p $HOME/.config/nvim
