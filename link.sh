f46c7a5bdcee1fba8f18d629d39e1155c7f80c08
andregoetz <andregoetz37@gmail.com>
2022-03-25 20:28:29 +0100

    add prettierrc

--- link.sh
+++ link.sh
@@ -5,4 +5,5 @@ p=$(pwd)/$(dirname $0)
 ln -sf $p/.vimrc $HOME
 ln -sf $p/.vimrc $HOME/.config/nvim/init.vim
 ln -sf $p/.zshrc $HOME
+ln -sf $p/.prettierrc $HOME
 ln -sf $p/andiru.zsh-theme $HOME/.oh-my-zsh/custom/themes
