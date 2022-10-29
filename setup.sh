96c299a30ccf05086f7afd23e570a6be56e5f311
andregoetz <andregoetz@proton.me>
2022-10-29 12:27:33 +0200

    modify setup.sh
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-if [[ $1 == "-i" ]]; then
+if [[ $1 == "-p" ]]; then
     echo "Installing packages"
     sudo pacman -S zsh zoxide \
-	gvim neovim python-pynvim vim-spell-de vim-spell-en \
-	elinks encfs qpdf xclip
+    gvim neovim python-pynvim vim-spell-de vim-spell-en \
+    elinks encfs qpdf xclip
     yay -S ddgr ttf-meslo-nerd-font-powerlevel10k
+else
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "Installing zsh plugins"
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    echo "Creating nvim config folder"
+    mkdir -p $HOME/.config/nvim
 fi
-echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installing zsh plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-echo "Creating nvim config folder"
-mkdir -p $HOME/.config/nvim
