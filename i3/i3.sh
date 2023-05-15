0f45ef5a2ca3bbc0afdbd8072ba2394555e268b9
andregoetz <andregoetz@proton.me>
2023-05-15 15:15:28 +0200

    i3 fix
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- i3/i3.sh
+++ i3/i3.sh
@@ -8,5 +8,5 @@ else
 
     ln -sf "$p"/config ~/.config/i3/config
     ln -sf "$p"/i3blocks.conf ~/.config/i3/i3blocks.conf
-    ln -sf "$p"/powermenu ~/.config/i3/scripts/powermenu
+    ln -sf "$p"/powermenu.rasi ~/.config/rofi/powermenu.rasi
 fi
