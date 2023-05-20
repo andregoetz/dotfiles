02302b5e76f689f387a7ea793caddd3f12598aed
andregoetz <andregoetz@proton.me>
2023-05-20 11:42:19 +0200

    i3 betterlockscreen instead of i3-lock
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- i3/i3.sh
+++ i3/i3.sh
@@ -9,4 +9,5 @@ else
     ln -sf "$p"/config ~/.config/i3/config
     ln -sf "$p"/i3blocks.conf ~/.config/i3/i3blocks.conf
     ln -sf "$p"/powermenu.rasi ~/.config/rofi/powermenu.rasi
+    ln -sf "$p"/powermenu ~/.config/i3/scripts/powermenu
 fi
