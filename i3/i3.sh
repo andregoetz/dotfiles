2ff53b056fdf9be3f852db82672118cb58c13e7b
andregoetz <andregoetz@proton.me>
2023-09-17 00:23:27 +0200

    feat: default apps and more i3 stuff
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- i3/i3.sh
+++ i3/i3.sh
@@ -7,3 +7,4 @@ ln -sf "$p"/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -sf "$p"/powermenu.rasi ~/.config/rofi/powermenu.rasi
 ln -sf "$p"/powermenu ~/.config/i3/scripts/powermenu
 ln -sf "$p"/.Xresources ~/.Xresources
+ln -sf "$p"/mimeapps.list ~/.config/mimeapps.list
--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ if [[ $1 == "-p" ]]; then
     yay -S zsh zoxide neovim python-pynvim vim-spell-de vim-spell-en \
     encfs qpdf xclip ttf-meslo-nerd-font-powerlevel10k \
     betterlockscreen gnome-disk-utility visual-studio-code-bin gnome-keyring gnome-system-monitor gnome-calculator xorg-xev \
-    flameshot geoclue redshift copyq xss-lock firefox-developer-edition ripgrep-all \
-    mimeo flatpak htop element-desktop
+    geoclue redshift copyq xss-lock firefox-developer-edition ripgrep-all \
+    mimeo flatpak htop element-desktop docker docker-compose just
     flatpak install flathub io.github.spacingbat3.webcord com.github.tchx84.Flatseal io.gitlab.librewolf-community com.github.Eloston.UngoogledChromium \
     org.libreoffice.LibreOffice org.flameshot.Flameshot org.gimp.GIMP com.github.vladimiry.ElectronMail org.mozilla.Thunderbird com.github.jeromerobert.pdfarranger \
-    org.qbittorrent.qBittorrent org.gnome.SimpleScan com.belmoussaoui.Authenticator
+    org.qbittorrent.qBittorrent org.gnome.SimpleScan com.belmoussaoui.Authenticator org.videolan.VLC com.nextcloud.desktopclient.nextcloud org.gnome.Evince
 else
     echo "Installing Oh My Zsh"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
