f8f099591a620c090ad124c1eb3e76f0e0343a15
andregoetz <andregoetz@proton.me>
2023-09-16 22:32:26 +0200

    feat: more programms and fix i3bar
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- data.sh
+++ data.sh
@@ -8,3 +8,8 @@ rm -rf ~/Pictures && ln -sf ~/data/Pictures ~
 rm -rf ~/Videos && ln -sf ~/data/Videos ~
 ln -sf ~/data/DEV ~
 ln -sf ~/data/Sync ~
+ln -sf ~/data/Android ~
+ln -sf ~/data/VirtualBox\ VMs ~
+ln -sf ~/data/Coatsink ~
+ln -sf ~/data/.technic ~
+ln -sf ~/data/.minecraft ~
