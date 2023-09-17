ec9258b151fdb792338531eb8ccff521afae6336
andregoetz <andregoetz@proton.me>
2023-09-17 19:20:07 +0200

    feat: moving and sound stuff
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- i3/i3.sh
+++ i3/i3.sh
@@ -6,5 +6,7 @@ ln -sf "$p"/config ~/.config/i3/config
 ln -sf "$p"/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -sf "$p"/powermenu.rasi ~/.config/rofi/powermenu.rasi
 ln -sf "$p"/powermenu ~/.config/i3/scripts/powermenu
+ln -sf "$p"/volume ~/.config/i3/scripts/volume
+ln -sf "$p"/microphone ~/.config/i3/scripts/microphone
 ln -sf "$p"/.Xresources ~/.Xresources
 ln -sf "$p"/mimeapps.list ~/.config/mimeapps.list
