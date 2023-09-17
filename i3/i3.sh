36c584f2f50f6fadedc03797b74e2d5034f0f230
andregoetz <andregoetz@proton.me>
2023-09-17 23:19:13 +0200

    fix: move scripts to dir
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- i3/i3.sh
+++ i3/i3.sh
@@ -5,11 +5,11 @@ p=$(pwd)/$(dirname $0)
 ln -sf "$p"/config ~/.config/i3/config
 ln -sf "$p"/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -sf "$p"/powermenu.rasi ~/.config/rofi/powermenu.rasi
-ln -sf "$p"/powermenu ~/.config/i3/scripts/powermenu
-ln -sf "$p"/volume ~/.config/i3/scripts/volume
-ln -sf "$p"/microphone ~/.config/i3/scripts/microphone
-ln -sf "$p"/cpu_usage ~/.config/i3/scripts/cpu_usage
-ln -sf "$p"/time ~/.config/i3/scripts/time
+ln -sf "$p"/scripts/powermenu ~/.config/i3/scripts/powermenu
+ln -sf "$p"/scripts/volume ~/.config/i3/scripts/volume
+ln -sf "$p"/scripts/microphone ~/.config/i3/scripts/microphone
+ln -sf "$p"/scripts/cpu_usage ~/.config/i3/scripts/cpu_usage
+ln -sf "$p"/scripts/time ~/.config/i3/scripts/time
 ln -sf "$p"/.Xresources ~/.Xresources
 ln -sf "$p"/mimeapps.list ~/.config/mimeapps.list
 
