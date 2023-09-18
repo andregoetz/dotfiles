0721e7c52803a8f31c2a846d4ed425f6b7b2fce1
andregoetz <andregoetz@proton.me>
2023-09-18 20:35:57 +0200

    fix: actually suspend after 15mins and dcnnt and nvimpager
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- i3/i3.sh
+++ i3/i3.sh
@@ -10,6 +10,7 @@ ln -sf "$p"/scripts/volume ~/.config/i3/scripts/volume
 ln -sf "$p"/scripts/microphone ~/.config/i3/scripts/microphone
 ln -sf "$p"/scripts/cpu_usage ~/.config/i3/scripts/cpu_usage
 ln -sf "$p"/scripts/time ~/.config/i3/scripts/time
+ln -sf "$p"/scripts/suspend_after_lock ~/.config/i3/scripts/suspend_after_lock
 ln -sf "$p"/.Xresources ~/.Xresources
 ln -sf "$p"/mimeapps.list ~/.config/mimeapps.list
 
