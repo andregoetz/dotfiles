10bce7eac2abde09db09dfb4146d84026cffbd12
andregoetz <andregoetz@proton.me>
2023-09-30 22:14:14 +0200

    fix: link.sh
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- link.sh
+++ link.sh
@@ -5,6 +5,7 @@ p=$(pwd)/$(dirname $0)
 ln -sf $p/.xprofile $HOME/.xprofile
 ln -sf $p/.xprofile $HOME/.profile
 ln -sf $p/.vimrc $HOME/.vimrc
+mkdir -p $HOME/.config/nvim
 ln -sf $p/.vimrc $HOME/.config/nvim/init.vim
 ln -sf $HOME/.config/nvim $HOME/.config/nvimpager
 ln -sf $p/.tmux.conf $HOME/.tmux.conf
