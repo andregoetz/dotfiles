dc79df59a25a04857bad77547236f4a51c1ed70f
andregoetz <andregoetz@proton.me>
2023-10-21 16:05:49 +0200

    feat: monero and flameshot not from flatpak
    Signed-off-by: andregoetz <andregoetz@proton.me>

--- setup.sh
+++ setup.sh
@@ -10,11 +10,11 @@ if [[ $1 == "-p" ]]; then
     github-cli nodejs npm nvimpager xkblayout-state-git perl-anyevent-i3 perl-json-xs gnome-calendar \
     xdotool xsane python-pip python-pipx fbreader ansible steam badlion-client wine heroic-games-launcher-bin \
     texlive biber texlive-langgerman openjdk-src openjdk-doc jdk-openjdk jre-openjdk maven go visual-studio-code-bin \
-    yt-dlp qt5ct adwaita-qt5-git
+    yt-dlp qt5ct adwaita-qt5-git flameshot ani-cli monero-gui
     flatpak install flathub io.github.spacingbat3.webcord com.github.tchx84.Flatseal io.gitlab.librewolf-community com.github.Eloston.UngoogledChromium \
-    org.libreoffice.LibreOffice org.flameshot.Flameshot org.gimp.GIMP com.github.vladimiry.ElectronMail org.mozilla.Thunderbird com.github.jeromerobert.pdfarranger \
+    org.libreoffice.LibreOffice org.gimp.GIMP com.github.vladimiry.ElectronMail org.mozilla.Thunderbird com.github.jeromerobert.pdfarranger \
     org.qbittorrent.qBittorrent org.gnome.SimpleScan com.belmoussaoui.Authenticator org.videolan.VLC com.nextcloud.desktopclient.nextcloud org.gnome.Evince \
-    com.github.xournalpp.xournalpp com.sigil_ebook.Sigil org.inkscape.Inkscape com.github.micahflee.torbrowser-launcher
+    com.github.xournalpp.xournalpp com.sigil_ebook.Sigil org.inkscape.Inkscape com.github.micahflee.torbrowser-launcher org.gtk.Gtk3theme.Mint-Y-Dark-Purple
 else
     echo "Installing Oh My Zsh"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
